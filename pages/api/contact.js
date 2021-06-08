04f502636d45c327c6fab25ac73f0c009bc6c5e4
Shahriar Swim <shahriar2k18@gmail.com>
2021-06-08 11:45:22 +0600

    added oAuth2 for email sending

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,5 @@
 import nodemailer from "nodemailer";
+import { google } from "googleapis";
 
 export default async (req, res) => {
   const { subject, email, name, message } = req.body;
@@ -10,48 +11,66 @@ export default async (req, res) => {
     });
   }
 
-  const transporter = nodemailer.createTransport({
-    port: 465,
-    host: "smtp.gmail.com",
-    auth: {
-      user: process.env.NODE_MAILER_USER,
-      pass: process.env.NODE_MAILER_PASS,
-    },
-    secure: true,
-  });
+  const oAuth2Client = new google.auth.OAuth2(
+    process.env.CLIENT_ID,
+    process.env.CLIENT_SECRET,
+    process.env.REDIRECT_URI
+  );
+  oAuth2Client.setCredentials({ refresh_token: process.env.REFRESH_TOKEN });
 
-  const mailData = {
-    from: process.env.NODE_MAILER_USER,
-    to: process.env.EMAIL,
-    subject: `${subject}`,
-    text: message + "Sent from: " + email,
-    html: `<div>
-        <p>From: ${email}</p>
-        <p>Name: ${name}</p>
-        <p>Message: ${message}</p>
-      </div>`,
-  };
+  try {
+    const accessToken = await oAuth2Client.getAccessToken();
 
-  // verify connection configuration
-  transporter.verify((error, _) => {
-    if (error) {
-      return res.status(400).json({
-        status: "failed",
-        message: err.message,
-      });
-    }
-  });
-
-  // send mail
-  transporter.sendMail(mailData, (err, info) => {
-    if (err) {
-      return res.status(400).json({
-        status: "failed",
-        message: err.message,
-      });
-    }
-    return res.status(200).json({
-      status: "success",
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        type: "OAuth2",
+        user: process.env.NODE_MAILER_USER,
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        refreshToken: process.env.REFRESH_TOKEN,
+        accessToken: accessToken,
+      },
     });
-  });
+
+    const mailData = {
+      from: process.env.NODE_MAILER_USER,
+      to: process.env.EMAIL,
+      subject: `${subject}`,
+      text: message + "Sent from: " + email,
+      html: `<div>
+          <p>From: ${email}</p>
+          <p>Name: ${name}</p>
+          <p>Message: ${message}</p>
+        </div>`,
+    };
+
+    // verify connection configuration
+    await transporter.verify((error, _) => {
+      if (error) {
+        return res.status(400).json({
+          status: "failed",
+          message: err.message,
+        });
+      }
+    });
+
+    // send mail
+    await transporter.sendMail(mailData, (err, _) => {
+      if (err) {
+        return res.status(400).json({
+          status: "failed",
+          message: err.message,
+        });
+      }
+      return res.status(200).json({
+        status: "success",
+      });
+    });
+  } catch (error) {
+    return res.status(400).json({
+      status: "failed",
+      message: error.message,
+    });
+  }
 };
